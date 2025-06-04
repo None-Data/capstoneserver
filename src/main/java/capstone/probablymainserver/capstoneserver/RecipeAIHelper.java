a19144f36db2c87a9eaab4aebfe0f3e4bd5b4ba8
김도영 202010842 <63325146+dore1115@users.noreply.github.com>
2025-06-04 15:01:06 +0900

    api key 변경

--- src/main/java/capstone/probablymainserver/capstoneserver/RecipeAIHelper.java
+++ src/main/java/capstone/probablymainserver/capstoneserver/RecipeAIHelper.java
@@ -13,7 +13,7 @@ import org.json.simple.parser.JSONParser;
 
 public class RecipeAIHelper {//RecipeAIHelper.getRecipeFromAI(prompt)를 호출하면 기능을 사용할 수 있다.
 
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "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";
     private static final int MAX_TOKENS = 4096;
     
     /*
